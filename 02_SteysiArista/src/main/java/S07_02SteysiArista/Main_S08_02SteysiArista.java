4a1345cbdd1b924d959478691f3d09e9159a2ac6
SteysiArista <steysi.arista@vallegrande.edu.pe>
2023-10-03 09:26:19 -0500

    Add files via upload

---
+++ 02_SteysiArista/src/main/java/S07_02SteysiArista/Main_S08_02SteysiArista.java
@@ -0,0 +1,20 @@
+
+package S07_02SteysiArista;
+
+import javax.swing.JOptionPane;
+
+public class Main_S08_02SteysiArista {
+            int patas;
+            int edad;
+    
+    public void patas(){
+        patas = Integer.parseInt(JOptionPane.showInputDialog("ingresar el primer numero"));
+    }
+        public void edad(){
+        patas = Integer.parseInt(JOptionPane.showInputDialog("ingresar el primer numero"));
+    }
+    
+    public int retorno(int patas, int edad){
+        return patas + edad;
+    }
+    }
